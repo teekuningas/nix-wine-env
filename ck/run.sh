369da9de517c25dde27ee7de3c3e2f7681da8304
Erkka Heinilä <erkkahe@gmail.com>
2023-02-21 11:07:08 +0200

    Update ck config

--- ck/run.sh
+++ ck/run.sh
@@ -1,8 +1,8 @@
 SCRIPT=`realpath $0`
 FOLDER=`dirname $SCRIPT`
 
-EU_PATH="$FOLDER/.wine/drive_c/GOG Games/Europa Universalis III"
-EU_FNAME="eu3game.exe"
+EU_PATH="$FOLDER/.wine/drive_c/GOG Games/Crusader Kings Complete"
+EU_FNAME="Crusaders.exe"
 
 cd "$EU_PATH"
 WINEPREFIX=$FOLDER/.wine wine $EU_FNAME
